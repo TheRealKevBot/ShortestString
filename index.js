696422dd06d9bfcefde71b6d18b4a5de294054d5
TheRealKevBot <Kglas001@gmail.com>
2021-06-11 20:38:23 -0600

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+
+function findShort(s){
+
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"), 'Answer: 3');
+console.log(findShort("turns out random test cases are easier than writing out basic ones"), 'Answer: 3'); 
+console.log(findShort("Let's travel abroad shall we"), 'Answer: 2");
